99e9ff3a6beb9515259ac63ea997f3d757b2e1a3
BahlbiR <70931490+BahlbiR@users.noreply.github.com>
2021-12-29 13:28:39 -0500

    Update enemy_projectile.py

--- enemy_projectile.py
+++ enemy_projectile.py
@@ -18,9 +18,11 @@ class Enemy_Projectile(pg.sprite.Sprite):
         screen.blit(self.image, self.rect)
 
     def update(self, delta):
+        # If the enemy projectile misses the player, remove it from the screen
         self.rect.x -= 1000 * delta
         if self.rect.x < 0:
             self.kill()
+        # If an enemy projectile hits the player, subtract 1 from their health
         collision = pg.sprite.collide_mask(self, self.player)
         if collision:
             self.player.health -= 1
